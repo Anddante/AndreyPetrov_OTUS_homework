5f2dabc7110f7e7c6b22ed4e5b377c04dd0a04dd
Andrew <84309874+Anddante@users.noreply.github.com>
2021-10-25 17:09:22 +0700

    Исправление ошибок

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function getPath(element) {
     let tags = [];
     while (element.tagName !== 'HTML') {
         if (element.hasAttribute('id')) {
-            tags.push(`#${element.id}`);
+            tags.unshift(`#${element.id}`);
         } else if (element.hasAttribute('class') && element.className) {
             tags.push('.' + element.className.split(' ').join('.'))
         } else {
-            tags.push(element.tagName.toLowerCase());
+            tags.push(':nth-child'+(element.tagName.toLowerCase());
         }
         element = element.parentElement
     }
@@ -14,3 +14,4 @@ function getPath(element) {
     path = tags.join(' ')
     return document.querySelectorAll(path) > 0 ? null : path
 }
+
